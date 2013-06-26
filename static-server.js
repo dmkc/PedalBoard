e1cb09860bf77f08e8d71825ac4b35e60b34f43e
Dmitry Kichenko <dmitrykichenko@gmail.com>
2013-06-26 15:37:53 -0400

    Add Express app to serve static files and route to BB

---
+++ static-server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    app = express()
+
+app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname + '/static/index.html'))
+// Route all other URI's through index.html, where the
+// backbone router should pick things up
+app.get('/*', function(req, res) {
+  res.sendfile(__dirname + '/static/index.html');
+})
+
+app.listen(3000)
+
